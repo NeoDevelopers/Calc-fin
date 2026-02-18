731f516fcf33dab06178c331b99120e315f2c3a9
Petr <35457522+RelaxPV@users.noreply.github.com>
2026-02-18 15:29:33 +0300

    new20

---
+++ config.js
@@ -0,0 +1,25 @@
+const CONFIG = {
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxyfKEG2iVZBcc9ZTx0bLud8uQ2kUAADyJEAJrpODfC9wSRCnSAqNgHuP-F1CcQzpZXrw/exec",
+  PASSWORDS: {
+    'Владелец': '9999',
+    'Екатерина': '3333',
+    'Михаил': '1234',
+    'Рома': '2222',
+    'Дима': '1111'
+  },
+  DEFAULTS: {
+    baseMat: 1133,
+    acrylicSheet: 600,
+    laborMult: 1.5,
+    overheadPerM2: 1500,
+    setupFix: 100,
+    marginSmall: 5.3,
+    marginLarge: 3.8,
+    acrylicDiscMaxPct: 20,
+    baseDiscMaxPct: 12,
+    filmMult: 1.3,
+    complexMult: 1.4,
+    lightFix: 700,
+    lightM2: 2000
+  }
+};
